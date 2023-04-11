50b776c9ca49eaf47f2a673b06e6741437c34738
seokhun <seokhun.yun1@gmail.com>
2023-04-11 15:45:30 +0900

    first-commit

---
+++ NUMATOTelnet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace NUMATOTelnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TelnetComm telnet = new TelnetComm();
+            telnet.Run("192.168.123.98", 23);
+            
+            telnet.OpenSession();
+
+            //while (true)
+            //{
+            //    Thread.Sleep(1000);
+            //}
+            while (true)
+            {
+                telnet.SetCommand("gpio set 3");
+                Thread.Sleep(500);
+                telnet.SetCommand("gpio clear 3");
+                Thread.Sleep(500);
+            }
+
+
+        }
+    }
+}
